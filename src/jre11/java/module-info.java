e0d626036f8b6427ec11b6f3261744c2d46dda3a
GedMarc <ged_marc@hotmail.com>
2018-11-26 00:31:58 +0200

    Check the module for param names?

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -13,8 +13,6 @@ module com.jwebmp.guicedinjection {
 
 	requires com.fasterxml.jackson.core;
 	requires com.fasterxml.jackson.databind;
-	requires com.fasterxml.jackson.datatype.jdk8;
-	requires com.fasterxml.jackson.datatype.jsr310;
 	requires com.fasterxml.jackson.annotation;
 
 	requires java.logging;
