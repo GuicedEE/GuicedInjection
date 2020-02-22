d02b0b6eaf588d206844cc53ce09122e837b951b
GedMarc <ged_marc@hotmial.com>
2020-02-22 11:23:47 +0200

    Enable a few deserializations? May change

--- src/main/java/com/guicedee/guicedinjection/interfaces/LocalDateTimeDeserializer.java
+++ src/main/java/com/guicedee/guicedinjection/interfaces/LocalDateTimeDeserializer.java
@@ -8,18 +8,38 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class LocalDateTimeDeserializer
 		extends JsonDeserializer<LocalDateTime>
 {
 	public static String LocalDateTimeFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSS";
+	public static String LocalDateTimeFormat2 = "yyyy-MM-dd HH:mm:ss.SSSSSSSS";
+	public static String LocalDateTimeFormat3 = "yyyy-MM-dd HH:mm:ss";
 
 	@Override
 	public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException
 	{
 		String name = p.getValueAsString();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(LocalDateTimeFormat);
-		LocalDateTime time = LocalDateTime.parse(name, formatter);
+		DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern(LocalDateTimeFormat2);
+		DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern(LocalDateTimeFormat3);
+		LocalDateTime time = null;
+		try
+		{
+			time = LocalDateTime.parse(name, formatter);
+		}
+		catch (DateTimeParseException dtpe)
+		{
+			try
+			{
+				time = LocalDateTime.parse(name, formatter2);
+			}
+			catch (DateTimeParseException dtpe2)
+			{
+				time = LocalDateTime.parse(name, formatter3);
+			}
+		}
 		return time;
 	}
 }
